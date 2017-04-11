4d87bd83183a0d6992f5f883daf1eee27b10d2f3
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-11 14:10:33 -0700

    Added react router

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
   render () {
@@ -15,4 +16,4 @@ const query = gql`
 
 `;
 
-export default SongList;
+export default graphql(query)(SongList);
--- client/index.js
+++ client/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
+import App from './components/SongList';
 import SongList from './components/SongList';
 
 const client = new ApolloClient({});
 
 const Root = () => {
   return 
-    <ApolloProvider>
-      <SongList />
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}> 
+        <Route path="/" compnent={App}>
+          <IndexRoute component={SongList}/>
+        </Route> 
+      </Router>
     </ApolloProvider>
 };
 
