84d28b5afd90072ece49f608e3876774b8f689fa
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-11 11:44:47 -0700

    added react component

--- client/index.js
+++ client/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
+import SongList from './components/SongList';
+
+const client = new ApolloClient({});
+
 const Root = () => {
-  return <div>Lyrical</div>
+  return 
+    <ApolloProvider>
+      <SongList />
+    </ApolloProvider>
 };
 
 ReactDOM.render(
